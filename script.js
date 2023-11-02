37f3b224084b8d691379ade6fdf8121d89c02538
Hvitrevs <venusof12pm@proton.me>
2023-11-02 15:23:09 -0400

    setting up canvas

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  const canvas = this.document.getElementById('canva');
+  const c = canvas.getContext('2d');
+  canvas.width = 800;
+  canvas.height = 800;
+})
