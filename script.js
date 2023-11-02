eb439ce2c5162003888b74d99e2a4a99c721e264
Hvitrevs <venusof12pm@proton.me>
2023-11-02 16:02:02 -0400

    canvas prop

--- script.js
+++ script.js
@@ -11,14 +11,26 @@ class Planet {
   draw(context){
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.fill();
+    context.stroke();
   }
 }
 
+
+class Game {
+  constructor(canvas){
+    this.canvas = canvas;
+
+  }
+}
 
 window.addEventListener('load', function() {
   const canvas = this.document.getElementById('canva');
   const c = canvas.getContext('2d');
   canvas.width = 800;
   canvas.height = 800;
+  c.strokeStyle = 'aquamarine'
+  c.lineWidth = 2;
+
+  const planet = new Planet();
+  planet.draw(c);
 })
