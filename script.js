ce6026991bc9c837300095a56d49df4c3e1b3453
Hvitrevs <venusof12pm@proton.me>
2023-11-02 15:58:04 -0400

    planet draw function

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+
+
+
+class Planet {
+  constructor(){
+    this.x = 200;
+    this.y = 200;
+    this.radius = 80;
+  }
+  draw(context){
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  }
+}
+
+
 window.addEventListener('load', function() {
   const canvas = this.document.getElementById('canva');
   const c = canvas.getContext('2d');
