56369d4eae10458547432ba22c145bc9cdb4142f
Hvitrevs <venusof12pm@proton.me>
2023-11-09 14:35:47 -0500

    get rid of out of vision items

--- script.js
+++ script.js
@@ -94,6 +94,11 @@ class Projectile {
       this.x += this.speedX;
       this.y += this.speedY;
     }
+
+    // reset outside visible game area
+    if(this.x < 0 || this.x > this.game.width || this.y < 0 || this.y > this.game.height){
+      this.reset();
+    }
   }
 }
 
